64abb6890e759420b5cd4080e6b322a94e5c6cb7
Peter121977 <piotr121977@gmail.com>
2023-05-24 23:54:06 +0000

    Add game result objects and get img elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-
+// game result set to 0 will update after every game
+const gameSummary = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0,
+  };
+  // object with live choise hand player and computer
+  const game = {
+    playerHand: "",
+    aiHand: "",
+  };
+  // take all img and convert to array, array has more methods than NodeList
+  const hands = [...document.querySelectorAll(".select img")];
   
   // first function which hand is selected
   function handSelection() {
